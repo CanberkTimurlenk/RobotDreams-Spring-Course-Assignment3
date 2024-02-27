7c40f30b2f1f73c2724f8d01277f1e0d7f0d1066
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2024-02-27 15:52:36 +0300

    First Commit

---
+++ src/main/java/com/robotdreams/assignment3/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.robotdreams.assignment3.service;
+
+import com.robotdreams.assignment3.dto.ProductResponseDto;
+import com.robotdreams.assignment3.repository.ProductMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ProductResponseDto> getProductsByCategory(Optional<String> categoryName) {
+
+        List<ProductResponseDto> query;
+
+        if (categoryName.isEmpty())
+            query = jdbcTemplate.query("SELECT * FROM Products", new ProductMapper());
+
+        else
+            // write a query to fetch products by category from the db
+            query = jdbcTemplate.query("SELECT * FROM Products WHERE Category = ?"
+                    , new Object[]{categoryName}, new ProductMapper());
+
+
+        return query;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Laptop', 'Electronic Devices', 'www.example1.com', 'Description for Laptop', 1200);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Tablet', 'Electronic Devices', 'www.example2.com', 'Description for Tablet', 800);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Smartphone', 'Electronic Devices', 'www.example3.com', 'Description for Smartphone', 600);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Smartwatch', 'Wearables', 'www.example4.com', 'Description for Smartwatch', 300);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Camera', 'Photography', 'www.example5.com', 'Description for Camera', 700);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Headphones', 'Audio Devices', 'www.example6.com', 'Description for Headphones', 150);
+
+INSERT INTO Products (name, category, photo_url, description, price)
+VALUES ('Gaming Laptop', 'Gaming Devices', 'www.example7.com', 'Description for Gaming Laptop', 2000);
